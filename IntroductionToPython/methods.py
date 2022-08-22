68b432730b7f2196eeff3ba1c8100d531c0948de
maruf <maruf@metageek.net>
2022-08-22 11:17:27 +0600

    Methods

---
+++ IntroductionToPython/methods.py
@@ -0,0 +1,14 @@
+# string to experiment with: place
+# methods: functions associated with an object
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up=place.upper()
+
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count('o'))
