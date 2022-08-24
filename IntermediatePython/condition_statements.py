d0193f3de97273d05de2c6f9fc0a6f79ec451f83
maruf <maruf@metageek.net>
2022-08-25 00:05:05 +0600

    Apply

--- IntermediatePython/condition_statements.py
+++ IntermediatePython/condition_statements.py
@@ -4,9 +4,9 @@ for x in range(1000) :
 		print(x)
 		print( 'which is even')
 		print("")
-		time.sleep(1/10)
+		time.sleep(1/100)
 	else :
 		print(x)
 		print( 'which is not even')
 		print("")
-		time.sleep(1/10)
+		time.sleep(1/100)
