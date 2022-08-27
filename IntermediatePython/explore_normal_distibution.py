11f163ad052d6a02efa29e2b15fbe71d34128e31
maruf <marufahmed@u.boisestate.edu>
2022-08-28 02:58:16 +0600

    Test Randomization

--- IntermediatePython/explore_normal_distibution.py
+++ IntermediatePython/explore_normal_distibution.py
@@ -1,28 +1,10 @@
-"""# NumPy is imported, seed is set
 import numpy as np
-# Starting step
-step = 50
+import matplotlib.pyplot as plt
+x = []
+y = 100000
+while y > 1 :
+    x.append(np.random.randint(1,100))
+    y = y-1
 
-# Roll the dice
-dice = np.random.randint(1,7)
-
-# Finish the control construct
-if dice <= 2 :
-    step = step - 1
-elif dice > 2 and dice < 6 :
-    step = step + 1
-else :
-    step = step + np.random.randint(1,7)
-
-# Print out dice and step
-print(dice)
-print(step)
-"""
-import numpy as np 
-x = 1000
-while x > 1 :
-	print(x)
-	x = x -1
-	y = np.random.randint(0,x)
-	print(',')
-	print(y)
+print(x)
+plt.hist(x)
