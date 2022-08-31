2a60690fc0daa5ed1e009c00224c7810256e50b7
maruf <marufahmed@u.boisestate.edu>
2022-08-31 20:55:29 +0600

    Next Planned Update

--- IntermediatePython/explore_normal_distibution.py
+++ IntermediatePython/explore_normal_distibution.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#needs to have an input from user to set feed and number of iterations and check the distribution
 x = []
 y = 100000
 while y > 1 :
